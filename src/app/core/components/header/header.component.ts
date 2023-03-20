553aa49b44761d827b254bfc2a091847f0df6b64
Pierre Etrillard <pierre.etrillard@live.fr>
2023-03-20 14:57:42 +0100

    calibrage des breakpointsdans les styles globaux

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from '../base/base.component';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+// constructor(baseComponent:BaseComponent){}
+// sectionSel = baseComponent.sectionSelector("accueil")
 }
