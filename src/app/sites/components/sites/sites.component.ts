2f44e0f022a129ec548d823f59404c75a5ce2170
Pierre ETRILLARD <pierre.etrillard@live.fr>
2023-05-09 17:35:27 +0200

    lien vers les sites de présentation et créations des variables d'environements

--- src/app/sites/components/sites/sites.component.ts
+++ src/app/sites/components/sites/sites.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { environment  } from "../../../../environments/environment.development";
 @Component({
   selector: 'app-sites',
   templateUrl: './sites.component.html',
   styleUrls: ['./sites.component.scss']
 })
 export class SitesComponent {
-
+creusardUrl:string=environment.creusardUrl
 }
