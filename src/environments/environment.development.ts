4b5d4b6e77fa993d3baa1d8cd075cbd6d9802a4e
Pierre ETRILLARD <pierre.etrillard@live.fr>
2023-05-27 11:19:12 +0200

    premier build du projet

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production:false,
+    creusardUrl:'http://localhost:4201'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production:true,
+    creusardUrl:'https://clickonaute.fr/demo/creusard'
+};
