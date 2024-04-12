b0b29e4825e97e9a0bf5b72d1cfb3bd0c60aa712
Pierre ETRILLARD <pierre.etrillard@live.fr>
2024-04-12 18:12:02 +0200

    build de la version pour git-hub pages

--- docs/common.7f6a402a4c64d02b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkclickonaute_fr=self.webpackChunkclickonaute_fr||[]).push([[592],{4466:(t,e,a)=>{a.d(e,{m:()=>r});var n=a(6895),o=a(8786),u=a(8256);let r=(()=>{class _{}return _.\u0275fac=function(c){return new(c||_)},_.\u0275mod=u.oAB({type:_}),_.\u0275inj=u.cJS({imports:[n.ez,o.Bz]}),_})()}}]);
