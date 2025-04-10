18308412164c1faa68b7fc6bc5eaed11fb1d3f43
Pierre ETRILLARD <pierre.etrillard@live.fr>
2025-04-10 18:50:23 +0200

    mise à jour de la logique de validation d'envoi de messsage

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { of, switchMap, tap } from 'rxjs';
 import { Disponibility } from 'src/app/features/agenda/disponibility.model';
 import { environment } from 'src/environments/environment.development';
 
@@ -11,10 +11,22 @@ export class ApiService {
   apiUrl = environment.apiUrl
   constructor(private http: HttpClient) {}
   unavailabilities$ = this.http.get<Date[]>(`${this.apiUrl}/periods/indispos`);
-  sendMail(message: any) {
-    return this.http.post(`${this.apiUrl}/message`, message, {
-      headers: new HttpHeaders({}),
-      responseType: 'text', 
+  getBadge(senderName: string) {
+    return this.http.post(`${this.apiUrl}/auth/badge`, senderName, {
+      responseType: 'text',
+      withCredentials: true
     });
+  }
+  sendMail(message: any) {
+    return this.getBadge(message.sender).pipe(
+      switchMap(() => {
+        console.log('Switching to message API call');
+        return this.http.post(`${this.apiUrl}/message`, message, {
+          headers: new HttpHeaders({}),
+          responseType: 'text',
+          withCredentials: true
+        })
+      })
+    );
   }
 }
